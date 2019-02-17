906ec90ae0c261920c4a83ada08436af45963c12
Wales Chang <pc963kimo@gmail.com>
2019-02-16 20:53:32 -0700

    test: remove finbug plugin for building

--- src/main/java/ca/wchang/openMoives/security/JwtTokenUtil.java
+++ src/main/java/ca/wchang/openMoives/security/JwtTokenUtil.java
@@ -1,6 +1,6 @@
 package ca.wchang.openMoives.security;
 
-import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+//import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Clock;
 import io.jsonwebtoken.Jwts;
@@ -21,7 +21,7 @@ public class JwtTokenUtil implements Serializable {
 
     static final String CLAIM_KEY_USERNAME = "sub";
     static final String CLAIM_KEY_CREATED = "iat";
-    @SuppressFBWarnings(value = "SE_BAD_FIELD", justification = "It's okay here")
+//    @SuppressFBWarnings(value = "SE_BAD_FIELD", justification = "It's okay here")
     private Clock clock = DefaultClock.INSTANCE;
 
     @Value("${jwt.secret}")
